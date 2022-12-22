e53e5ab26741d01d9c968d63b50b81fedd6f7fad
Max Glass <glassmax13@gmail.com>
2022-12-22 14:54:17 -0500

    Move daisyui to dev dependencies and add react-daisyui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/daisyui/dist/**/*.js",
+    "node_modules/react-daisyui/dist/**/*.js",
+  ],
   theme: {
     extend: {
       colors: {
