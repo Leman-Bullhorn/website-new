24775275c4e4cd2c765015fea3b3e67ca8fa6d85
Max Glass <glassmax13@gmail.com>
2023-05-23 23:49:03 -0400

    Remove unused daisyui styles being includes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "node_modules/daisyui/dist/**/*.js",
-    "node_modules/react-daisyui/dist/**/*.js",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
